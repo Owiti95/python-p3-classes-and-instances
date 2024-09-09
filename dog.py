60d48006dba68dbf5b7c814bcc3a1648f1aa6228
hannington owiti <hanningtonowiti@gmail.com>
2024-09-09 06:30:25 +0300

    classes and instances

---
+++ dog.py
@@ -0,0 +1,14 @@
+#remember that python requires some indented code in each code block.
+#instead of empty blocks, we use the "pass" keyword to do nothing.
+
+class Dog:
+    pass
+
+fido = Dog()
+fido
+
+snoopy = Dog()
+snoopy
+
+lassie = Dog()
+lassie
